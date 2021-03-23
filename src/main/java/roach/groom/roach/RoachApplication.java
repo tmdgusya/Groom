86d79953c5c911cc925e701d466d9d43e42b36a5
tmdgusya <tmdgusya@naver.com>
2021-03-23 14:21:13 +0900

    #1 add Dependency

---
+++ src/main/java/roach/groom/roach/RoachApplication.java
@@ -0,0 +1,13 @@
+package roach.groom.roach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/roach/groom/roach/RoachApplicationTests.java
@@ -0,0 +1,13 @@
+package roach.groom.roach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
